5069224dba64496e5a9109b59897431157028c24
fisa0003 <159115696+fisa0003@users.noreply.github.com>
2024-06-11 19:13:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      // Toggle the active class on the accordion link
+      this.classList.toggle("active");
+
+      // Get the next sibling element, which should be the panel
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+});
